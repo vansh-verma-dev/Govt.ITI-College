36e724a2e653e9da6edb0c110986abb6fb3059af
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-05 19:18:30 +0530

    initial add navbar and hero section upgarde UI & UX desgin

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
- console.log('welcom to college');
- 
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav-foot");
+const links = document.querySelectorAll(".nav-foot a");
+
+/* open close button */
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  document.body.classList.toggle("menu-open");
+});
+
+/* close when link clicked */
+links.forEach(link=>{
+  link.addEventListener("click",()=>{
+    nav.classList.remove("active");
+    document.body.classList.remove("menu-open");
+  });
+});
+
+/* outside click close */
+document.addEventListener("click",(e)=>{
+  if(!nav.contains(e.target) && !menuBtn.contains(e.target)){
+    nav.classList.remove("active");
+    document.body.classList.remove("menu-open");
+  }
+});
