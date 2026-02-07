0b022a4d74b0f270144fb1a2067ff92a7a8409b5
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-07 12:10:57 +0530

    add admission & facilitys section with UI & UX desgin

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 const menuBtn = document.querySelector(".menu-btn");
-const nav = document.querySelector(".nav-foot");
-const links = document.querySelectorAll(".nav-foot a");
+const navMenu = document.getElementById("navMenu");
 
- 
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("active");
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navMenu.classList.toggle("active");
   document.body.classList.toggle("menu-open");
 });
 
- 
-links.forEach(link=>{
-  link.addEventListener("click",()=>{
-    nav.classList.remove("active");
-    document.body.classList.remove("menu-open");
-  });
+document.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  document.body.classList.remove("menu-open");
 });
 
- 
-document.addEventListener("click",(e)=>{
-  if(!nav.contains(e.target) && !menuBtn.contains(e.target)){
-    nav.classList.remove("active");
-    document.body.classList.remove("menu-open");
-  }
+navMenu.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
