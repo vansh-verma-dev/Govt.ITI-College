a1aa77d4454e41c4a183df695f7519e3cbc9e690
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-22 23:06:37 +0530

    add contact us page function using Js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const overlay = document.getElementById("overlay");
 
+const Username = document.getElementById('name');
+const email = document.getElementById('email');
+const msg = document.getElementById('msg');
+
+
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   overlay.classList.toggle("active");
@@ -11,3 +16,9 @@ overlay.addEventListener("click", () => {
   navLinks.classList.remove("active");
   overlay.classList.remove("active");
 });
+
+function submit(){
+  const nameinput = Username.value.trim();
+  const emailinput = email.value.trim();
+  const msginput =  msg.value.trim();
+}
